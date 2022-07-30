367813e3e6c174723bdf82783c97e57add77a8a6
trungAGG <105260835+trungAGG@users.noreply.github.com>
2022-07-30 19:28:17 +0700

    Initial commit

---
+++ movieApp/src/app/pages/movies/movies.page.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+import { MovieService } from 'src/app/services/movie.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+  movies = [];
+  currentPage = 1;
+  imageBaseUrl = environment.images
+  constructor(private movieService: MovieService, private loadingCrtl: LoadingController) { }
+
+  ngOnInit() {
+    this.loadMovies();
+  }
+  async loadMovies(event?) {
+    const loading = await this.loadingCrtl.create({
+      message: 'Loading movies...',
+      spinner: 'bubbles',
+    })
+    await loading.present();
+    this.movieService.getTopRatedMovies(this.currentPage).subscribe(res => {
+      loading.dismiss()
+      this.movies.push(...res.results)
+      console.log(res)
+      event?.target.complete();
+      if (event) {
+        event.target.disabled = res.total_pages === this.currentPage
+      }
+    })
+  }
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.currentPage++
+    this.loadMovies(event)
+  }
+
+}
